7b053ffc4da3964fdeac6047b8180bd212cbe62a
arpil_khunt <arpilkhunt1203@gmail.com>
2024-08-22 21:34:37 +0530

    add testing data

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const Listing = require("./models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -18,6 +18,19 @@ async function main() {
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root.");
+});
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the beach",
+    price: 2000,
+    location: "Manvilas,Bhavnagar,Gujarat",
+    country: "India",
+  });
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("Successful testing");
 });
 
 app.listen(8080, () => {
