800414d0c2d2db1c1c173488800b946077479c11
arpil_khunt <arpilkhunt1203@gmail.com>
2024-08-23 21:43:33 +0530

    add new changes

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.engine("ejs", ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 main()
   .then(() => {
