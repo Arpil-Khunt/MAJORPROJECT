4268421d6ae01938290c63997454ae193b178957
arpil_khunt <arpilkhunt1203@gmail.com>
2024-08-29 20:00:54 +0530

    handle the error

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const ejs = require("ejs");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 //set the view engine to ejs
 app.set("view engine", "ejs");
@@ -36,53 +38,93 @@ app.get("/", (req, res) => {
 });
 
 //Index route
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("./listings/index.ejs", { allListings });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", { allListings });
+  })
+);
 
 //New route
-app.get("/listings/new", (req, res) => {
-  res.render("./listings/new.ejs");
-});
+app.get(
+  "/listings/new",
+  wrapAsync((req, res) => {
+    res.render("./listings/new.ejs");
+  })
+);
 
 //show route
-app.get("/listings/:id", async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("./listings/show.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/show.ejs", { listing });
+  })
+);
 
 //Create route
-app.post("/listings", async (req, res) => {
-  // let { title, description, price, image, country, location } = req.body;
-  // console.log(title, description, price, image, country, location);
-  let listing = req.body.listing;
-  const newListing = new Listing(listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    // let { title, description, price, image, country, location } = req.body;
+    // console.log(title, description, price, image, country, location);
+
+    //handle the bad request means if listing is not available then how you store listing in the database
+    if (!req.body.listing) {
+      throw new ExpressError(400, "send valid data for listing");
+    }
+    let listing = req.body.listing;
+    const newListing = new Listing(listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit route
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  let listing = await Listing.findById(id);
-  res.render("./listings/edit.ejs", { listing });
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs", { listing });
+  })
+);
 
 //Update route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let listing = req.body.listing;
-  await Listing.findByIdAndUpdate(id, listing);
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "send valid data for listing");
+    }
+    let { id } = req.params;
+    let listing = req.body.listing;
+    await Listing.findByIdAndUpdate(id, listing);
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 //Delete route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect("/listings");
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  })
+);
+
+//send a stadard response for all route, if there is not exist => * means all route if the route is exist then it match above if not then come this route it match all incoming route
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "page not found!"));
+});
+
+//serverside custome error handling -> error handling middleware
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong!" } = err;
+  res.status(statusCode).send(message);
 });
 
 // app.get("/testListing", async (req, res) => {
